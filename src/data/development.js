50c97e07e48c536b7f44c5f223d9c84f72c20b6c
Tarek Allam <tarek.allam@live.com>
2021-07-22 11:27:39 -0400

    fix compile error

--- src/data/development.js
+++ src/data/development.js
@@ -1,3 +1,5 @@
-export default {
+const server = {
     servicea: "http://localhost:8080/",
 }
+
+export default server;
--- src/data/production.js
+++ src/data/production.js
@@ -1,3 +1,5 @@
-export default {
+const server = {
     servicea: "http://52.14.96.41:8080/",
 }
+
+export default server;
